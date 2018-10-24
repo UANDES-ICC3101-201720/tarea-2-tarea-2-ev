6fcd48c099e86bad98fc5eea1acb5eb058ccceb8
eivelez <eivelez@miuandes.cl>
2018-10-24 05:01:47 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -15,9 +15,91 @@ Loop infinito (solucionado)
 Método implementado no funciona (Eliminado)
 incoherencias en resultados (solucionado)
 segmentation fault(en revision)
+modificar input programa con flags (REVISAR)
 
 23/10:
-modificar input programa con flags (REVISAR)
-uninitialized in this function [-Wmaybe-uninitialized] (solucionado)
+-Empezar todo denuevo
+-uninitialized in this function [-Wmaybe-uninitialized] (solucionado)
+-Error de uso de memoria en la funcion de añadir nodo (solucioando)
+-Error causado por uso de Do While en vez de While(solucionado)
+-Error causado por uso de recursividad en las funciones (solucionado)
 
 Informe:
+
+Para esta tarea se debió implementar un algoritmo de reemplazo de páginas. Método FIFO, Random y uno custom creado por nosotros; elegí uno simple y totalmente arbitrario y sin intención de eficiencia. Un método circular en el cual libera el ultimo, penultimo, antepenultimo, hasta llegar al primero para comenzar denuevo, es una especie de fifo alreves por lo que a veces eliminara al mas nuevo y otras al mas viejo.
+
+Si tratara de predecir los resultados mi custom se comportara similar al FIFO y el random tendra comportamientos diferentes a los dos. no sé si en algunos casos mejores o peores.
+
+Luego de probar cada uno de los algoritmos con los diferentes programas obtuve los siguiente resultados:
+
+
+
+------------------------------------------------------------------------------------------
+				         SCAN
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 custom scan
+scan result is 522240000
+Pages Faults: 1080
+Disk Writes: 1065
+Disk Reads: 1080
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 fifo scan
+scan result is 522240000
+Pages Faults: 1100
+Disk Writes: 1085
+Disk Reads: 1100
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 random scan
+scan result is 522240000
+Pages Faults: 1099
+Disk Writes: 1084
+Disk Reads: 1099
+
+
+
+
+
+					FOCUS
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 custom focus
+focus result is -2533
+Pages Faults: 281
+Disk Writes: 266
+Disk Reads: 281
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 fifo focus
+focus result is -2533
+Pages Faults: 279
+Disk Writes: 264
+Disk Reads: 279
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 random focus
+focus result is -2533
+Pages Faults: 283
+Disk Writes: 268
+Disk Reads: 283
+
+
+
+
+					SORT
+
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 custom sort
+sort result is -311357
+Pages Faults: 797
+Disk Writes: 782
+Disk Reads: 797
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 fifo sort
+sort result is -5576384
+Pages Faults: 726
+Disk Writes: 711
+Disk Reads: 726
+pbn@pbn-VirtualBox:~/Desktop/Tarea2/tarea-2-tarea-2-ev-master$ ./virtmem 100 15 random sort
+sort result is -311357
+Pages Faults: 883
+Disk Writes: 868
+Disk Reads: 883
+
+
+--------------------------------------------------------------------------------------------
+
+Con estos resultados podemos ver que custom no genera tanta falta de paginas en scan y podría llegar a ser más ultil en comparacion a rand y fifo.
+En sort random tuvo el peor desempeño seguido de lejos por custom, siendo el mejor fifo.
+En focus los resutados fueron similares, debera compararse para otra cantidad de frames, y no se llega a conclusion clara.
+
+Podemos concluir de este trabajo que no hay métodos mejores ni peores que otros, dependera de la tarea que se quiera realizar. Tambien aprendí a la fuerza que WHILE no es lo mismo que DO-WHILE aun que son parecidos, error que me hizo tener que partir todo denuevo.
+
--- main.c
+++ main.c
@@ -1,71 +1,217 @@
-/*
-Main program for the virtual memory project.
-Make all of your modifications to this file.
-You may add or rearrange any code or data as you need.
-The header files page_table.h and disk.h explain
-how to use the page table and disk interfaces.
-*/
-
 #include "page_table.h"
 #include "disk.h"
 #include "program.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
-void page_fault_handler( struct page_table *pt, int page )
+int counterP,diskR,diskW = 0;
+char *virtmem;
+char *physmem;
+int *auxArray;
+char *argv3;
+int auxdta;
+int auxdta2;
+int auxPag;
+int npages;
+int nframes;
+struct disk *disk;
+
+typedef struct Nodo
 {
-	printf("page fault on page #%d\n",page);
-	exit(1);
+	int pagina;
+	int dta;
+	struct Nodo * siguiente;
+} Nodo;
+
+typedef struct Tabla
+{
+	Nodo * inicial;
+}Tabla;
+
+Tabla *data;
+//no sirve
+/*void Next(Nodo * node)
+{
+	node=node->siguiente;
+}*/
+int GetRandom(int min,int max)
+{
+	int dif=max-min;
+	int random=lrand48()%dif;
+	random=min+random;
+	return random;
+	
+}
+int GetPagina(Nodo *actual,int auxdta)//No sé por que no funciona con recursividad, fué cambiado por loop
+{
+	while (actual!=NULL)
+	{
+		if (actual->dta==auxdta)
+		{
+			return actual->pagina;
+		}
+		actual=actual->siguiente;
+	}
+	return -1;
+}
+/*void Imprimir()
+{
+	Nodo* actual=data->inicial;
+	while (actual!=NULL)
+	{
+		printf("%d\n",actual->pagina);printf("%d\n",actual->dta);printf("\n");
+		actual=actual->siguiente;
+	}
+}*/
+void CambiarPagina(Nodo *actual,int inputdta,int inputPagina)
+{
+	if(actual==NULL){}
+	else if(actual->dta==inputdta){actual->pagina=inputPagina;}
+	else if(actual->dta!=inputdta){CambiarPagina(actual->siguiente,inputdta,inputPagina);}
+}
+/*void SacarUltimo()
+{
+	new=data->inicial;
+	while(new->siguiente!=NULL){new=new->siguiente;}
+	new->siguiente=malloc(sizeof(Nodo));
+	new=NULL;
+}
+*/
+void AgregarNodo(Nodo *new,int inputdta,int inputpagina)
+{
+	new=data->inicial;
+	while(new->siguiente!=NULL){new=new->siguiente;}
+	new->siguiente=malloc(sizeof(Nodo));
+	new=new->siguiente;
+	new->pagina=inputpagina;
+	new->dta=inputdta;
+}
+void EliminarPrimerNodo(){data->inicial = data->inicial->siguiente;}
+/*void SacarX(int i)
+{
+	new.....
+*/
+
+//-------------------------------------------------------------------------------------------------//
+// MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK //
+//------------------------------------------------------------------------------------------------//
+void page_fault_handler(struct page_table *pt, int page)
+{                          
+	int activar=0;counterP++;int auxC=0;	
+	for (int i=0;i<nframes;i++)
+	{
+	if(auxArray[i]!=-1)
+	{
+	//printf("Error en page_fault_handler");"
+	diskR++;disk_read(disk,page,&physmem[i*PAGE_SIZE]);
+	page_table_set_entry(pt,page,i,PROT_READ|PROT_WRITE|PROT_EXEC);
+	Nodo *new=data->inicial;AgregarNodo(new,i,page);
+	auxC++;activar=1;auxArray[i]=-1;
+	break;
+	}
+	}
+//-------------------------------------------------------------------------------------------------//
+// MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK MARK //
+//------------------------------------------------------------------------------------------------// 
+
+
+//METhod codes//
+	if (activar==0)
+	{
+		if (!strcmp(argv3, "fifo"))
+		{
+			auxdta = data->inicial->dta;
+			auxPag = data->inicial->pagina;
+			EliminarPrimerNodo();
+			Nodo *new = data->inicial;AgregarNodo(new,auxdta,page);
+			diskW++;disk_write(disk,auxPag,&physmem[auxdta*PAGE_SIZE]);
+			diskR++;disk_read(disk, page, &physmem[auxdta*PAGE_SIZE]);
+			
+		}
+		if (!strcmp(argv3, "custom"))//ciclico
+		{
+			auxdta2--;
+			if(auxdta2==1){auxdta2=nframes-1;}
+			auxdta=auxdta2;
+			auxPag = GetPagina(data->inicial,auxdta);
+			diskW++;disk_write(disk,auxPag, &physmem[auxdta*PAGE_SIZE]);
+			diskR++;disk_read(disk, page, &physmem[auxdta*PAGE_SIZE]);
+			CambiarPagina(data->inicial,auxdta,page);
+		}
+		if (!strcmp(argv3, "random"))
+		{
+			auxdta =GetRandom(0,nframes);
+			auxPag = GetPagina(data->inicial,auxdta);
+			diskW++;disk_write(disk,auxPag, &physmem[auxdta*PAGE_SIZE]);
+			diskR++;disk_read(disk, page, &physmem[auxdta*PAGE_SIZE]);
+			CambiarPagina(data->inicial,auxdta,page);
+			
+		}
+		
+		page_table_set_entry(pt,auxPag,auxdta, 0);
+		page_table_set_entry(pt, page,auxdta,PROT_READ|PROT_WRITE|PROT_EXEC);
+	}
 }
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-	if(argc!=5) {
-		/* Add 'random' replacement algorithm if the size of your group is 3 */
-		printf("use: virtmem <npages> <nframes> <lru|fifo> <sort|scan|focus>\n");
-		return 1;
-	}
+	
+	argv3 = argv[3];
+	data = malloc(sizeof(Tabla));
+	data->inicial = malloc(sizeof(Nodo));
+	npages = atoi(argv[1]);
+	nframes = atoi(argv[2]);
+	auxdta2=nframes-1;
+	int aux[nframes];
+	auxArray=aux;
+	for(int j=0;j<nframes;++j){auxArray[j]=j;}
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
 	const char *program = argv[4];
 
-	struct disk *disk = disk_open("myvirtualdisk",npages);
-	if(!disk) {
-		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
+	disk = disk_open("myvirtualdisk", npages);
+	if (!disk)
+	{
+		fprintf(stderr, "couldn't create virtual disk: %s\n", strerror(errno));
 		return 1;
 	}
 
+	struct page_table *pt = page_table_create(npages, nframes, page_fault_handler);
 
-	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
-	if(!pt) {
-		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
+	if (!pt)
+	{
+		fprintf(stderr, "couldn't create page table: %s\n", strerror(errno));
 		return 1;
 	}
 
-	char *virtmem = page_table_get_virtmem(pt);
-
-	char *physmem = page_table_get_physmem(pt);
-
-	if(!strcmp(program,"sort")) {
-		sort_program(virtmem,npages*PAGE_SIZE);
-
-	} else if(!strcmp(program,"scan")) {
-		scan_program(virtmem,npages*PAGE_SIZE);
-
-	} else if(!strcmp(program,"focus")) {
-		focus_program(virtmem,npages*PAGE_SIZE);
-
-	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
-
+	virtmem = page_table_get_virtmem(pt);
+	physmem = page_table_get_physmem(pt);
+	for (int i=0; i < nframes; ++i)
+	{
+		disk_write(disk, i, &physmem[i * BLOCK_SIZE]);
 	}
 
+	if (!strcmp(program, "sort"))
+	{
+		//printf("Error en sort_program");"
+		sort_program(virtmem, npages * PAGE_SIZE);
+	}
+	else if (!strcmp(program, "scan"))
+	{
+		scan_program(virtmem, npages * PAGE_SIZE);
+	}
+	else if (!strcmp(program, "focus"))
+	{
+		focus_program(virtmem, npages * PAGE_SIZE);
+	}
+	else
+	{
+		fprintf(stderr, "unknown program: %s\n", argv[4]);
+	}
+	printf("Pages Faults: %i\nDisk Writes: %i\nDisk Reads: %i\n", counterP, diskW, diskR);
 	page_table_delete(pt);
 	disk_close(disk);
-
-	return 0;
+	free(data);
+	return 1;
 }
